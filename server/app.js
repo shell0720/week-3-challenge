4e1adb76069e69d7f005ff380e2b6c391cae852f
shell0720 <shell0720@gmail.com>
2016-03-14 12:07:00 -0500

    thrid edition

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 var index = require('./routes/index.js');
 var bodyParser = require('body-parser');
+
+
+app.set("port", (process.env.PORT || 3000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("/", index);
 
-app.listen(3000,function(){
-  console.log("listening on port:3000");
+app.listen(app.get("port"),function(){
+  console.log("listening on port:", app.get("port"));
 
 });
